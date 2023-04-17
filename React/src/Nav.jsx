bb799fb696eacb2cd86bfcc67fa029a9d306951b
Weerawut Chaiyasomboon <weerawut@live.com>
2023-04-17 16:48:30 +0700

    Add Bootstrap Nav

--- React/src/Nav.jsx
+++ React/src/Nav.jsx
@@ -1,13 +1,25 @@
+import fitbook from './assets/FITBOOK.png';
+import './Nav.css';
+
 const Navbar = () => {
     return (
-        <ul>
-            <li><a href={'/'}>Main</a></li>
-            <li><a href={'/Login'}>Login</a></li>
-            <li><a href={'/Signup'}>Signup</a></li>
-            <li><a href={'/Create'}>Create Activity</a></li>
-            <li><a href={'/Update'}>Upadate Activity</a></li>
-            <li><a href={'/Delete'}>Delete Activity</a></li>
-        </ul>
+        <div className="container">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom customBackgroundColor">
+          <a href={'/'} className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <img src={fitbook} alt="fitbook" width="300" height="50" />
+          </a>
+    
+          <ul className="nav">
+            <li className="nav-item"><a href={'/'} className="nav-link text-white" aria-current="page">Main</a></li>
+            <li className="nav-item"><a href={'/Login'} className="nav-link text-white">Log in</a></li>
+            <li className="nav-item"><a href={'/Signup'} className="nav-link text-white">Sign up</a></li>
+            <li className="nav-item"><a href={'/Create'} className="nav-link text-white">Create</a></li>
+            <li className="nav-item"><a href={'/Update'} className="nav-link text-white">Update</a></li>
+            <li className="nav-item"><a href={'/Delete'} className="nav-link text-white">Delete</a></li>
+          </ul>
+        </header>
+    </div>
+
     )
 }
 
