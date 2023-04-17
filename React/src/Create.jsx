ae1d963447958796b9403f96749a6c5fb7c5c88d
anutpri <anut.pri@gmail.com>
2023-04-17 13:00:58 +0700

    create Create component

---
+++ React/src/Create.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout'
+
+const Create = () => {
+    return (
+        <Layout>
+            <div>
+                Create
+            </div>
+        </Layout>
+    )
+}
+
+export default Create
