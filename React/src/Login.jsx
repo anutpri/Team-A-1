64156093681b7788ef48d82523d1caf75f0b7443
anutpri <anut.pri@gmail.com>
2023-04-17 15:57:09 +0700

    update Login

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
             <input type="text" placeholder="Type Email here" style={{margin: '4px'}} onChange={(event) =>setEmail(event.target.value)} /><br></br><br></br>
             <label>Password</label> <br></br>
             <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
-            <button onClick={() => login({ username: email, password: password})}>Login</button>
+            <button onClick={() => login({})}>Login</button>
             </div>
             
         </Layout>
