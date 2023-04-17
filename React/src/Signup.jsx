974d51ebda70410c080cdd3ced9f66db3891092b
anutpri <anut.pri@gmail.com>
2023-04-17 15:15:43 +0700

    update Signup

--- React/src/Signup.jsx
+++ React/src/Signup.jsx
@@ -5,25 +5,24 @@ import React, { useState } from 'react'
 
 const Signup = () => {
     
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
     const [fullname, setFullname] = useState();
-    const [organization, setOrganization] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    
     // const { signup } = useContext(Context)
   
 
     return (
         <Layout>
             <div>
-            <label>Username</label> <br></br>
-            <input type="text" placeholder="Type Username here" style={{margin: '4px'}} onChange={(event) =>setUsername(event.target.value)} /><br></br><br></br>
-            <label>Password</label> <br></br>
-            <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
             <label>Fullname</label> <br></br>
             <input type="text" placeholder="Type fullname here" style={{margin: '4px'}} onChange={(event) =>setFullname(event.target.value)} /><br></br><br></br>
-            <label>Organization</label> <br></br>
-            <input type="text" placeholder="Type organization here" style={{margin: '4px'}} onChange={(event) =>setOrganization(event.target.value)} /><br></br><br></br>
-            <button onClick={() => signup({ username: username, password: password, fullname: fullname, organization: organization})}>Save</button>
+            <label>Email</label> <br></br>
+            <input type="text" placeholder="Type Email here" style={{margin: '4px'}} onChange={(event) =>setEmail(event.target.value)} /><br></br><br></br>
+            <label>Password</label> <br></br>
+            <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
+            
+            <button onClick={() => signup({ username: email, password: password, fullname: fullname})}>Save</button>
             </div>
         </Layout>
     )
