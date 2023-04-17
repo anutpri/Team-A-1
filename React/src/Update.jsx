5e0b8f2ed8c5171e37253f6518af7364180a258b
anutpri <anut.pri@gmail.com>
2023-04-17 13:02:01 +0700

    create Update component

---
+++ React/src/Update.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout'
+
+const Update = () => {
+    return (
+        <Layout>
+            <div>
+            Update
+            </div>
+        </Layout>
+    )
+}
+
+export default Update
