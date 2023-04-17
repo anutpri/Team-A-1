54480ec816e4e836427ed9ffffab4b60481b3c8d
anutpri <anut.pri@gmail.com>
2023-04-17 14:51:25 +0700

    update Login & Signup

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -1,11 +1,26 @@
-import Layout from './Layout'
+import Layout from "./Layout"
+// import { useContext } from "react"
+// import { Context } from "./UserContext"
+import React, { useState } from 'react'
 
 const Login = () => {
+    
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    // const { login } = useContext(Context)
+
+    
+  
     return (
         <Layout>
             <div>
-                Login
+            <label>Username</label> <br></br>
+            <input type="text" placeholder="Type Username here" style={{margin: '4px'}} onChange={(event) =>setUsername(event.target.value)} /><br></br><br></br>
+            <label>Password</label> <br></br>
+            <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
+            <button onClick={() => login({ username: username, password: password})}>Login</button>
             </div>
+            
         </Layout>
     )
 }
--- React/src/Signup.jsx
+++ React/src/Signup.jsx
@@ -1,10 +1,29 @@
-import Layout from './Layout'
+import Layout from "./Layout"
+// import { Context } from "./UserContext"
+// import { useContext } from "react"
+import React, { useState } from 'react'
 
 const Signup = () => {
+    
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [fullname, setFullname] = useState();
+    const [organization, setOrganization] = useState();
+    // const { signup } = useContext(Context)
+  
+
     return (
         <Layout>
             <div>
-            Signup
+            <label>Username</label> <br></br>
+            <input type="text" placeholder="Type Username here" style={{margin: '4px'}} onChange={(event) =>setUsername(event.target.value)} /><br></br><br></br>
+            <label>Password</label> <br></br>
+            <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
+            <label>Fullname</label> <br></br>
+            <input type="text" placeholder="Type fullname here" style={{margin: '4px'}} onChange={(event) =>setFullname(event.target.value)} /><br></br><br></br>
+            <label>Organization</label> <br></br>
+            <input type="text" placeholder="Type organization here" style={{margin: '4px'}} onChange={(event) =>setOrganization(event.target.value)} /><br></br><br></br>
+            <button onClick={() => signup({ username: username, password: password, fullname: fullname, organization: organization})}>Save</button>
             </div>
         </Layout>
     )
