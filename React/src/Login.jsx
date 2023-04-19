8a9c8c52b196900c76b6a2f715058cb5b91f4ed3
anutpri <anut.pri@gmail.com>
2023-04-19 23:45:09 +0700

    finish login code and css

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -1,25 +1,54 @@
 import Layout from "./Layout"
-// import { useContext } from "react"
-// import { Context } from "./UserContext"
 import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import './Login.css'
 
 const Login = () => {
     
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    // const { login } = useContext(Context)
-
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState(''); 
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
     
+   
+    // Array of user data for testing
+    const userData = [
+        { email: 'user1@gmail.com', password: 'password1' },
+        { email: 'user2@gmail.com', password: 'password2' },
+        { email: 'user3@gmail.com', password: 'password3' }
+    ];
+
+    const handleLogin = (event) => {
+        event.preventDefault(); 
+        const user = userData.find(user => user.email === email && user.password === password); // Check for a user email and password match the userData mockup
+        if(user) {
+            // If a user is found, the login is successful and redirected to App (main)
+            alert('Login successful!');
+            navigate("/");
+
+        } else {
+            // If a user is not found, display an error message 
+            setError('Invalid email or password');
+        }
+    }
   
     return (
         <Layout>
-        
-            <div>
-            <label>Email</label> <br></br>
-            <input type="text" placeholder="Type Email here" style={{margin: '4px'}} onChange={(event) =>setEmail(event.target.value)} /><br></br><br></br>
-            <label>Password</label> <br></br>
-            <input type="password" placeholder="Type Password here" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br><br></br>
-            <button onClick={() => login({})}>Login</button>
+            <div class="header">
+                <div class="title">
+                <p>Sign In to</p>
+                <p>Change</p>
+                <p>Your Life</p> <br />
+                <p id='if'>If you don't an account <br></br><span>you can</span> <a class="Regis" href={'/Signup'}>Register Here!</a></p>
+                 </div>
+            </div>
+            <div class = "Login">
+            {/* <label>Email</label> <br></br> */}
+            <input type="text" placeholder="Enter Email" style={{margin: '4px'}} onChange={(event) =>setEmail(event.target.value)} /><br></br><br></br>
+            <input type="password" placeholder="Password" style={{margin: '4px'}} onChange={(event) =>setPassword(event.target.value)} /><br></br>
+            <a class = "Forgot">Forgot Password?</a><br></br><br></br>
+            {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display the error message */}
+            <button onClick={handleLogin}>Sign In</button>
             </div>
             
         </Layout>
