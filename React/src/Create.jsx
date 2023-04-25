5332d634ec69207903ee5c8bc557ed6be0f32ec7
anutpri <anut.pri@gmail.com>
2023-04-25 17:13:05 +0700

    edit activity list

--- React/src/Create.jsx
+++ React/src/Create.jsx
@@ -19,8 +19,8 @@ const Create = () => {
         {id:1,type:'Running'},
         {id:2,type:'Walking'},
         {id:3,type:'Bike cycling'},
-        {id:4,type:'weight training'},
-        {id:5,type:'dancing'},
+        {id:4,type:'Weight training'},
+        {id:5,type:'Dancing'},
         ];
 
 
