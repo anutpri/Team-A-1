633636a99e3bdfc68d6997d1e5210aab902010e3
anutpri <anut.pri@gmail.com>
2023-04-25 23:47:49 +0700

    created mock database in App.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Layout from './Layout'
 import Create from './Create';
@@ -6,6 +6,13 @@ import Edit from './Edit';
 
 function App() {
   
+  //Mock database on localhost
+  const [userActivity, setUserActivity] = useState([]);
+
+  useEffect(() => {
+    const storedActivity = JSON.parse(localStorage.getItem('userActivity')) || [];
+    setUserActivity(storedActivity);
+  }, []);
 
   return (
     <Layout>
