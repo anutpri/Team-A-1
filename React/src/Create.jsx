a860d1cc41359c41ec2260f88410fa2f7f7c2f3f
anutpri <anut.pri@gmail.com>
2023-05-02 22:41:14 +0700

    finish comment fro Create.jsx

--- React/src/Create.jsx
+++ React/src/Create.jsx
@@ -15,6 +15,8 @@ const Create = () => {
     const [durationTime, setDurationTime] = useState('');
     const [distance, setDistance] = useState('');
     const [error, setError] = useState(null);
+    
+    //Mockup data for activities list
     const activityTypeList = [
         {id:1,type:'Running'},
         {id:2,type:'Walking'},
@@ -40,35 +42,48 @@ const Create = () => {
 
     const handleAddUserActivity = () => {
     
+        // check if activityName is empty
         if (!activityName.trim()) {
             setError('Activity name is required');
             return;
-          }
-          if (!description.trim()) {
+        }
+
+        // check if description is empty
+        if (!description.trim()) {
             setError('Description is required');
             return;
-          }
-          if (new Date(startDateTime)< new Date()) {
+        }
+
+        // check if startDateTime is before current date-time
+        if (new Date(startDateTime)< new Date()) {
             setError('Start date-time must be after current date-time');
             return;
-          }
-          if (!startDateTime || !finishDateTime) {
+        }
+
+        // check if startDateTime or finishDateTime is empty
+        if (!startDateTime || !finishDateTime) {
             setError('Start and Finish date-time are required');
             return;
-          }
-          if (new Date(startDateTime) >= new Date(finishDateTime)) {
+        }
+
+        // check if finishDateTime is before startDateTime
+        if (new Date(startDateTime) >= new Date(finishDateTime)) {
             setError('Finish date-time must be after start date-time');
             return;
-          }
-          if (!activityType) {
+        }
+
+        // check if activityType is empty
+        if (!activityType) {
             setError('Activity type is required');
             return;
-          }
-          if (!durationTime || durationTime < 10) {
+        }
+
+        // check if durationTime is empty or less than 10
+        if (!durationTime || durationTime < 10) {
             setError('Duration time is required and should be at least 10 minutes');
             return;
           }
-
+    // create a new user activity object
     const newUserActivity = {
           id: Date.now(),
           activityName,
@@ -79,20 +94,27 @@ const Create = () => {
           durationTime,
           distance
         };
-        setUserActivity([...userActivity, newUserActivity]);
-        localStorage.setItem('userActivity', JSON.stringify([...userActivity, newUserActivity]));
 
+        // add the new user activity object to the userActivity state array
+        setUserActivity([...userActivity, newUserActivity]);
+        // save the updated userActivity state array to localStorage
+        localStorage.setItem('userActivity', JSON.stringify([...userActivity, newUserActivity]));
+        // clear the input form fields
         clearDataForm();
+        // show a success message
         alert('Save successful!');
+        // navigate to the Dashboard page
         navigate('/Dashboard');     
       };
 
+    // Define a function to handle the cancel button click event
     const handleCancel= () => {
         clearDataForm();
         navigate('/Dashboard'); 
       };
 
     const clearDataForm= () => {
+      // Set all state values to empty strings
         setActivityName('');
         setDescription('');
         setStartDateTime('');
@@ -101,6 +123,8 @@ const Create = () => {
         setDurationTime('');
         setDistance('');
         setError('');
+
+        // Reset the input fields to empty strings
         acName.value = '';
         descript.value = '';
         start.value = '';
@@ -115,31 +139,47 @@ const Create = () => {
     return (
         <div className='Create'>
         <header>
+          {/* The header section */}
           <a href={'/'}>
-            <img src={fitbook} alt="fitbook"/>
+            {/* Link to the homepage */}
+            <img src={fitbook} alt="fitbook" />
+            {/* Logo image */}
           </a>
         </header>
             <h2 id='header1'>CREATE A NEW CARD</h2>
             <div className='inputData'>
             
             <label>Activity Name</label> <br></br>
+            {/* setActivityName when input onChange */}
             <input id="acName" type="text" onChange={(event) =>setActivityName(event.target.value)} /><br></br><br></br>
+
             <label>Description</label> <br></br>
+            {/* setDescription when input onChange */}
             <input id="descript" type="text" onChange={(event) =>setDescription(event.target.value)} /><br></br><br></br>
+
+            
             <label id='startL'>Start-DateTime</label> <label id='finishL'>Finish-DateTime</label><br></br>
+            {/* setStartDateTime when input onChange */}
             <input id="start" type="datetime-local" placeholder="YYYY-MM-DD:HH:MM:SS" onChange={(event) =>setStartDateTime(event.target.value)} />
+           {/* setFinishDateTime when input onChange */}
             <input id="finish" type="datetime-local" placeholder="YYYY-MM-DD:HH:MM:SS" onChange={(event) =>setFinishDateTime(event.target.value)} /><br></br><br></br><br></br>
             
             <label id='acTypeL'>Activity Type</label> <label id='setDuL'>Duration Time</label> <label id="setDisL">Distance       </label>
+            {/* setActivityType when input onChange */}
             <select id="acType" onChange={(event) => setActivityType(event.target.value)}>
+
             <option value="">-- Select --</option>
+            {/* Map data from activityTypeList to dropdown input */}
             {activityTypeList.map((activity) => (<option key={activity.id} value={activity.type}>{activity.type}</option>))}
             </select>
+            {/* setDurationTime when input onChange */}
             <input id="setDu" placeholder="Minutes" min="10" step="10" type="number" onChange={(event) =>setDurationTime(event.target.value)} />
+            {/* setDistance when input onChange */}
             <input id="setDis" placeholder="kilometer" min="0.0" step="0.1" type="number" onChange={(event) =>setDistance(event.target.value)} /><br></br><br></br>
+            
             {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display the error message */}
-            <button id="save" onClick={handleAddUserActivity}>Save</button>
-            <button id="cancel" onClick={handleCancel}>Cancel</button>
+            <button id="save" onClick={handleAddUserActivity}>Save</button> {/* Call handleAddUserActivity when click */}
+            <button id="cancel" onClick={handleCancel}>Cancel</button> {/* Call handleCancel when click */}
             
             </div>
         </div>
