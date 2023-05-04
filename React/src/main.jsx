b6aca9090a97ea4ddd082f6e51b648996d7e2479
NutyadaS <nutyada.s7810@gmail.com>
2023-05-04 15:08:25 +0200

    Create feature card & add to nav bar

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,51 +1,50 @@
-
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import Create from './Create';
 import Edit from './Edit';
 import Dashboard from './Dashboard';
+import Card from './Card';
 
 const router = createBrowserRouter([
   // insert your path here
   {
     path: '/',
-    element: <App />
+    element: <App />,
   },
   {
     path: '/Login',
-    element: <Login />
+    element: <Login />,
   },
   {
     path: '/Signup',
-    element: <Signup />
+    element: <Signup />,
   },
   {
     path: '/Dashboard',
-    element: <Dashboard />
+    element: <Dashboard />,
   },
   {
     path: '/Create',
-    element: <Create />
+    element: <Create />,
   },
   {
     path: '/Edit',
-    element: <Edit />
+    element: <Edit />,
   },
-  
-])
+  {
+    path: '/Card',
+    element: <Card />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
