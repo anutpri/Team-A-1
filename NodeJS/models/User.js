38e0b5df1c1d1418e53052c3a2143fc898cb5e8b
anutpri <anut.pri@gmail.com>
2023-05-09 15:29:52 +0700

    update trim: true for User.js

--- NodeJS/models/User.js
+++ NodeJS/models/User.js
@@ -10,12 +10,14 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true
     },
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true
     },
   password: {
     type: String,
