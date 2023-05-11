e95d260203fdd2cf1c29bcd0d0cc898093f9fd33
anutpri <anut.pri@gmail.com>
2023-05-11 14:37:51 +0700

    update node.js

--- React/src/api/Node.js
+++ React/src/api/Node.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const PORT = 3000;
+const PORT = 8080;
 const HOSTNAME = "http://127.0.0.1";
 const SERVER = `${HOSTNAME}:${PORT}`;
 
 
 export const getUser = async () => {
   // code down below
-  const response = await axios.get(`${SERVER}/user`);
+  const response = await axios.get(`${SERVER}/users`);
   const body = response.data;
   return body;
 };
 
 export const createUser = async (body) => {
   // code down below
-  const response = await axios.post(`${SERVER}/newuser`, body);
+  const response = await axios.post(`${SERVER}/users`, body);
   alert(response.data);
 };
