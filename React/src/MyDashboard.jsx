c0a6fd52de7dae5cd58c469f8c6cf40acab3efd1
NutyadaS <nutyada.s7810@gmail.com>
2023-05-18 15:54:45 +0200

    update the card not overlapping

--- React/src/MyDashboard.jsx
+++ React/src/MyDashboard.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import Activities from "./Card";
-import Profile from "./Profile";
-import Layout from "./Layout";
-import { userData } from "./api/Session";
+import React from 'react';
+import Activities from './Card';
+import Profile from './Profile';
+import Layout from './Layout';
+import { userData } from './api/Session';
 
-export default function MyDashboard(){
-    
-    return(
-        <Layout>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-5">
-                        <Profile/>
-                        
-                    </div>
-                    <div className="col-sm-7">
-                        <Activities/>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
+export default function MyDashboard() {
+  return (
+    <Layout>
+      <div className='d-flex justify-content-center container-fluid'>
+        <div className='col'>
+          <div className='col-md'>
+            <Profile />
+          </div>
+          <br />
+          <div className='col-md'>
+            <Activities />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
-
