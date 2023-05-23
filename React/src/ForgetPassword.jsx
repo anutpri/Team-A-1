fe247636f961a45648f03792719bbb26c44be049
anutpri <anut.pri@gmail.com>
2023-05-23 10:19:30 +0700

    navigate submit forgot password to main

--- React/src/ForgetPassword.jsx
+++ React/src/ForgetPassword.jsx
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import Layout from "./Layout";
 import forget from "./assets/forget.jpg";
 import styles from "./ForgetPassword.module.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function ForgetPassword() {
   const [email, setEmail] = useState();
+  const navigate = useNavigate();
 
   const handleForgetFormSubmit = (event) => {
     event.preventDefault();
 
     alert(`Sorry for the inconvenience. This feature is under development; Please take a deep breath and re-think your password again.`);
     setEmail('');
+    navigate('/');
   };
 
   return (
