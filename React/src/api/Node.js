f70c2a6f50fa52b0d4ba848f9f63bebfbf2f4447
anutpri <anut.pri@gmail.com>
2023-05-23 15:45:45 +0700

    change server domain

--- React/src/api/Node.js
+++ React/src/api/Node.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // const PORT = import.meta.env.DATABASE_PORT;
- const HOSTNAME = import.meta.env.DATABASE_IP;
- const SERVER = `${HOSTNAME}`;
+//  const HOSTNAME = import.meta.env.DATABASE_IP;
+//  const SERVER = `${HOSTNAME}`;
 
-// const SERVER = "http://127.0.0.1:8080";
+const SERVER = "https://fitbookserver.onrender.com;
 
 export const getUser = async () => {
   try {
